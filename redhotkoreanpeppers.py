9d29b7ee7aec0c8a48beb8c4619baacb7ff49f43
woogieboogie <woogieboogie.jl@gmail.com>
2021-02-24 18:27:23 +0900

    hotfix

--- redhotkoreanpeppers.py
+++ redhotkoreanpeppers.py
@@ -1,10 +1,9 @@
 def webscraper_test():
-    for i in range (1,10)
+    for i in range (1,10):
         print("scraping data")
 
 while True:
     try:
         print("redhotkoreanpeppers")
     except EOFError:
-
-    
+    	pass
