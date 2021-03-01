85c9b1fe8f7f3a0dc6ddd04efba65c53f256a253
jaewooklee <woogieboogie.jl@gmail.com>
2021-03-01 20:02:33 +0900

    learn javascript

---
+++ jstest.js
@@ -0,0 +1,8 @@
+const myTag = document.querySelector('.color-btn')
+console.log(myTag)
+
+const myTags = document.querySelectorAll('.color-btn')
+console.log(myTags)
+
+const myTagsByClasName = document.getElementsByClassName('color-btn')
+console.log(myTagsByClasName)
